d089f8143ca02ae572b67dfb4c68dab20281907a
TmLev <sololev27@gmail.com>
2021-12-30 20:51:17 +0300

    feat: Implement inspection

--- src/details/impl.cpp
+++ src/details/impl.cpp
@@ -1,6 +1,7 @@
 #include <details/impl.hpp>
 
 #include <clang/AST/ASTContext.h>
+#include <clang/AST/Attr.h>
 #include <clang/AST/Stmt.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 
@@ -11,6 +12,7 @@
 #include <llvm/Support/Casting.h>
 
 #include <memory>
+#include <variant>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -23,25 +25,81 @@ constexpr auto kSwitchStmtID = "switch_stmt_id";
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class MatchCallback : public clang::ast_matchers::MatchFinder::MatchCallback {
- public:
-  using MatchResult = clang::ast_matchers::MatchFinder::MatchResult;
+ private:
+  struct ShouldSkip {};
 
-  auto run(const MatchResult& result) -> void override {
-    const auto switch_stmt = result.Nodes.getNodeAs<clang::SwitchStmt>(kSwitchStmtID);
+  struct MissingBreak {
+    [[maybe_unused]] const clang::Stmt* stmt_before_next_case;
+    [[maybe_unused]] const clang::SwitchCase* next_case;
+  };
+
+  // clang-format off
+  using InspectionResult = std::variant<
+      ShouldSkip,
+      MissingBreak
+  >;
+  // clang-format on
+
+ public:
+  auto run(const clang::ast_matchers::MatchFinder::MatchResult& match_result) -> void override {
+    const auto switch_stmt = match_result.Nodes.getNodeAs<clang::SwitchStmt>(kSwitchStmtID);
 
     for (const clang::SwitchCase* switch_case = switch_stmt->getSwitchCaseList();
          switch_case != nullptr; switch_case = switch_case->getNextSwitchCase()) {
-      if (ShouldSkipSwitchCase(switch_case)) {
+      const InspectionResult inspection_result = InspectSwitchCase(switch_case);
+
+      if (std::holds_alternative<ShouldSkip>(inspection_result)) {
         continue;
       }
+
+      [[maybe_unused]] const auto missing_break = std::get<MissingBreak>(inspection_result);
+
+      clang::DiagnosticsEngine& diagnostics = match_result.Context->getDiagnostics();
+      const unsigned id = diagnostics.getCustomDiagID(clang::DiagnosticsEngine::Warning,
+                                                      "Case is missing `break` statement");
+      clang::DiagnosticBuilder builder = diagnostics.Report(switch_case->getKeywordLoc(), id);
     }
   }
 
  private:
-  auto ShouldSkipSwitchCase(const clang::SwitchCase* switch_case) const -> bool {
-    const auto* as_default = llvm::dyn_cast<clang::DefaultStmt>(switch_case);
-    const auto* as_case = llvm::dyn_cast<clang::CaseStmt>(switch_case);
-    return true;
+  static auto InspectSwitchCase(const clang::SwitchCase* switch_case) -> InspectionResult {
+    const clang::Stmt* stmt_before_next_case = switch_case;
+    const clang::SwitchCase* next_case = nullptr;
+
+    for (const clang::Stmt* sub_stmt : switch_case->children()) {
+      // Case has fallen through into another case.
+      if (llvm::isa<clang::SwitchCase>(sub_stmt)) {
+        next_case = llvm::dyn_cast<clang::SwitchCase>(sub_stmt);
+        break;
+      }
+
+      // Case has `break` statement.
+      if (llvm::isa<clang::BreakStmt>(sub_stmt)) {
+        return ShouldSkip{};
+      }
+
+      // Case has `return` statement.
+      if (llvm::isa<clang::ReturnStmt>(sub_stmt)) {
+        return ShouldSkip{};
+      }
+
+      // Case has `[[fallthrough]]` attribute.
+      if (llvm::isa<clang::AttributedStmt>(sub_stmt)) {
+        const auto* attributed_stmt = llvm::dyn_cast<clang::AttributedStmt>(sub_stmt);
+        for (const clang::Attr* attribute : attributed_stmt->getAttrs()) {
+          if (llvm::isa<clang::FallThroughAttr>(attribute)) {
+            return ShouldSkip{};
+          }
+        }
+      }
+
+      stmt_before_next_case = sub_stmt;
+    }
+
+    return MissingBreak{
+        .stmt_before_next_case = stmt_before_next_case,
+        .next_case = next_case,
+    };
   }
 };
 
@@ -52,8 +110,7 @@ class Consumer : public clang::ASTConsumer {
   using MatchFinder = clang::ast_matchers::MatchFinder;
 
   Consumer() {
-    using namespace clang::ast_matchers;  // NOLINT
-    const auto matcher = switchStmt().bind(kSwitchStmtID);
+    const auto matcher = clang::ast_matchers::switchStmt().bind(kSwitchStmtID);
     match_finder_.addMatcher(matcher, &match_callback_);
   }
 
